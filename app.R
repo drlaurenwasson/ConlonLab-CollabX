2b14e4145d0c95062e69abd2f12bc9881df941cf
drlaurenwasson <dr.lauren.wasson@gmail.com>
2024-06-07 10:39:53 -0400

    20240607_branch

--- app.R
+++ app.R
@@ -54,10 +54,13 @@ ui <- fluidPage(theme=shinytheme("cerulean"),
                  ),
         tabPanel("Male/Female Ratio",
                  plotOutput("tab2expressionbarplot"),
-                 downloadButton("DownloadPlot2", "Download Plot")
+                 downloadButton("DownloadPlot2", "Download Plot"),
+                 plotOutput("tab3plot"),
+                 downloadButton("DownloadPlot3", "Download Plot"),
+                 tableOutput("tab3table"),
+                 downloadButton("DownloadTable", "Download Table")
                  ),
-        tabPanel("Male/Female Ratio vs Ref Strain")
-      ),
+                ),
     )
   )
 )
@@ -73,18 +76,15 @@ server <- function(input, output, session) {
   get_metadata<- reactive({
     proteinstouse<- input$proteinoi
     proteinstouse<-unlist(strsplit(proteinstouse, ","))
-    print(proteinstouse)
     return(proteinstouse)
   })
   get_strains<- reactive({
     strainstouse<- input$strains
     strainstouse<-unlist(strsplit(strainstouse, ","))
-    print(strainstouse)
     return(strainstouse)
   })
   get_normalized_strain<- reactive({
     norm<- input$normstrain
-    print(norm)
     return(norm)
   })
   
@@ -94,6 +94,7 @@ server <- function(input, output, session) {
   cc_df2<- read.table("https://github.com/drlaurenwasson/ConlonLab-CollabX/raw/main/files/cc_df2.txt", sep = "\t")
   colnames(cc_df2)[4]<- "129"
   cc_df3<- read.table("https://github.com/drlaurenwasson/ConlonLab-CollabX/raw/main/files/cc_df3.txt", sep = "\t")
+  colnames(cc_df3)[4]<- "129"
   #Get a data frame for the proteins of interest for the strains of interest
   getdata<- function(protein,strain){
     allstrains<- c("PWK", "C57", "AJ", "129", "WSB", "CAS", "NZO", "NOD")
@@ -125,7 +126,7 @@ server <- function(input, output, session) {
     #Update the strains to include
     strainsupdate<- get_strains()
     output$strainstouse<- shiny::renderText({strainsupdate})
-    #Update the time point to normalize to
+    #Update the strain to normalize to
     normalizeupdate<- get_normalized_strain()
     output$normalizedstrain<- shiny::renderText({normalizeupdate})
     #Get the data for the genes we want and the strains we want
@@ -136,6 +137,9 @@ server <- function(input, output, session) {
         a<- getdata(p,{strainsupdate})
         df=rbind(df,a)
       }
+      else{
+        output$proteinstouse<- shiny::renderText("")
+      }
       output$dataframe<- shiny::renderTable({
         df}, rownames = TRUE
       )
@@ -143,7 +147,6 @@ server <- function(input, output, session) {
     #Tab 1: Plot protein expression values
     #Subset only the strains you want
     df<- df[df$strain %in% {strainsupdate},]
-    print(df)
     #Plot the box plot
     p<- ggplot(df, aes(x=strain, y=Expression, group = ss, fill = ss)) + 
       geom_boxplot() + labs(title=paste0("Normalized Protein Expression- ", sort(unique(df$protein))), x="Strain", y = "Avg. Abundance (Normalized)")+theme_classic() 
@@ -175,22 +178,45 @@ server <- function(input, output, session) {
       plot(p2)
     })
     
-    #Tab 3: Normalize to the reference strain
-    df3<- df2
-    df3test<- as.data.frame(t(cc_df3[rownames(cc_df3)=={proteinsupdate},]))
-    df3test$strain<- colnames(cc_df3)
-    df3test$strain[4]<- "129"
-    df3test$strain<- factor(df3test$strain, levels=c("AJ","C57","129","NOD", "NZO", "CAS", "PWK", "WSB"))
-    colnames(df3test)[1]<-"Log2FC"
-    df3test$color<- c("#D55E00", "#555555", "#F0E442", "#E69F00", "#CC79A7", "#009E73", "#56B4E9", "#0072B2" )
+    output$DownloadPlot2 = downloadHandler(
+      file= paste({proteinsupdate}, "adjusted.png", sep=""),
+      content = function(file){
+        ggsave(p2, filename = file)
+      }
+    )
+    
+    #Tab 3: Normalize to the reference strain2
+    df3<- as.data.frame(t(cc_df3[rownames(cc_df3)=={proteinsupdate},]))
+    df3$strain<- colnames(cc_df3)
+    df3$strain[4]<- "129"
+    df3$strain<- factor(df3$strain, levels=c("AJ","C57","129","NOD", "NZO", "CAS", "PWK", "WSB"))
+    colnames(df3)[1]<-"Log2FC"
+    df3$color<- c("#D55E00", "#555555", "#F0E442", "#E69F00", "#CC79A7", "#009E73", "#56B4E9", "#0072B2" )
+    df3$adj<- df3$Log2FC/df3[rownames(df3)[rownames(df3) %in% {normalizeupdate}],1]
     #Subset the rows you want
-    df3test<- df3test[df3test$strain %in% {strainsupdate},]
-    p2<- ggplot(df3test,aes(x=strain, y=Log2FC))+
-      geom_bar(stat="identity", fill = df2$color ) 
-    p2<- p2+labs(title=paste0("Normalized Protein Expression- ", {proteinsupdate}), x="Strain", y = "Log2FC M/F")+
+    df3<- df3[df3$strain %in% {strainsupdate},]
+    p3<- ggplot(df3,aes(x=strain, y=adj))+
+      geom_bar(stat="identity", fill = df3$color ) 
+    p3<- p3+labs(title=paste0("Adjusted Normalized Protein Expression- ", {proteinsupdate}), x="Strain", y = "Adj Log2FC M/F")+
       theme_classic() 
-    output$tab2expressionbarplot <- renderPlot({
-      plot(p2)
+    output$tab3plot<- renderPlot({
+      plot(p3)
+    })
+    
+    output$DownloadPlot3 = downloadHandler(
+      file= paste({proteinsupdate}, ".png", sep=""),
+      content = function(file){
+        ggsave(p3, filename = file)
+      }
+    )
+    output$tab3table<- renderTable({df3})
+    
+    output$DownloadTable = downloadHandler(
+      file= paste({proteinsupdate}, ".txt", sep=""),
+      content = function(file){
+        write.table(df3, filename = file)
+      }
+    )
   })
 
 }
--- files/20240404_DataManipulation_CC.R
+++ files/20240404_DataManipulation_CC.R
@@ -36,10 +36,12 @@ colnames(pwk)<- c("F6_M_PWK", "F6_F_PWK", "F7_M_PWK", "F10_F_PWK")
 pwk2<-pwk
 pwk2$M_PWK_avg<- rowMeans(subset(pwk, select = c(F6_M_PWK, F7_M_PWK)), na.rm = TRUE)
 pwk2$F_PWK_avg<- rowMeans(subset(pwk, select = c(F6_F_PWK, F10_F_PWK)), na.rm = TRUE)
+
+pwk2$FCMvsF_PWK<- round((pwk2$M_PWK_avg/pwk2$F_PWK_avg), digits = 2)
 pwk2$Log2MvsF_PWK<- round(log2(pwk2$M_PWK_avg/pwk2$F_PWK_avg), digits = 2)
 
 #C57
-c57<- f6norm[,3]
+c57<- as.data.frame(f6norm[,3])
 rownames(c57)<- genes
 a<- f9norm[,c(3,9)]
 c57<- cbind(c57,a)
@@ -50,6 +52,7 @@ colnames(c57)<- c("F6_F_C57", "F9_F_C57", "F9_M_C57", "F10_M_C57")
 c572<- c57
 c572$M_C57_avg<- rowMeans(subset(c57, select = c(F9_M_C57, F10_M_C57)), na.rm = TRUE)
 c572$F_C57_avg<- rowMeans(subset(c57, select = c(F6_F_C57, F9_F_C57)), na.rm = TRUE)
+c572$FCMvsF_c57<- round((c572$M_C57_avg/c572$F_C57_avg), digits = 2)
 c572$Log2MvsF_c57<- round(log2(c572$M_C57_avg/c572$F_C57_avg), digits = 2)
 
 #A/J
@@ -63,14 +66,13 @@ colnames(aj)<- c("F6_F_AJ", "F7_M_AJ", "F9_F_AJ", "F9_M_AJ")
 aj2<- aj
 aj2$M_aj_avg<- rowMeans(subset(aj, select = c(F7_M_AJ, F9_M_AJ)), na.rm = TRUE)
 aj2$F_aj_avg<- rowMeans(subset(aj, select = c(F6_F_AJ, F9_F_AJ)), na.rm = TRUE)
+aj2$FCMvsF_aj<- round((aj2$M_aj_avg/aj2$F_aj_avg), digits = 2)
 aj2$Log2MvsF_aj<- round(log2(aj2$M_aj_avg/aj2$F_aj_avg), digits = 2)
 
 #129
-s129<- f6norm[,5]
+s129<- as.data.frame(f6norm[,5])
 a<- f7norm[,c(3,7)]
 s129<- cbind(s129,a)
-#a<- f9norm[,c(3,9)]
-#s129<- cbind(s129,a)
 rownames(s129)<- genes
 a<- f10norm[,6]
 s129<- cbind(s129,a)
@@ -78,6 +80,7 @@ colnames(s129)<- c("F6_M_129", "F7_M_129", "F7_F_129", "F10_F_129")
 s1292<- s129
 s1292$M_s129_avg<- rowMeans(subset(s129, select = c(F6_M_129, F7_M_129)), na.rm = TRUE)
 s1292$F_s129_avg<- rowMeans(subset(s129, select = c(F7_F_129, F10_F_129)), na.rm = TRUE)
+s1292$FCMvsF_s129<- round((s1292$M_s129_avg/s1292$F_s129_avg), digits = 2)
 s1292$Log2MvsF_s129<- round(log2(s1292$M_s129_avg/s1292$F_s129_avg), digits = 2)
 
 
@@ -92,6 +95,7 @@ colnames(wsb)<- c("F6_F_WSB", "F9_M_WSB", "F10_F_WSB", "F10_M_WSB")
 wsb2<- wsb
 wsb2$M_wsb_avg<- rowMeans(subset(wsb, select = c(F9_M_WSB, F10_M_WSB)), na.rm = TRUE)
 wsb2$F_wsb_avg<- rowMeans(subset(wsb, select = c(F6_F_WSB, F10_F_WSB)), na.rm = TRUE)
+wsb2$FCMvsF_wsb<- round((wsb2$M_wsb_avg/wsb2$F_wsb_avg), digits = 2)
 wsb2$Log2MvsF_wsb<- round(log2(wsb2$M_wsb_avg/wsb2$F_wsb_avg), digits = 2)
 
 #CAS
@@ -105,6 +109,7 @@ colnames(cas)<- c("F6_F_CAS", "F7_M_CAS", "F10_F_CAS", "F10_M_CAS")
 cas2<- cas
 cas2$M_cas_avg<- rowMeans(subset(cas, select = c(F7_M_CAS, F10_M_CAS)), na.rm = TRUE)
 cas2$F_cas_avg<- rowMeans(subset(cas, select = c(F6_F_CAS, F10_F_CAS)), na.rm = TRUE)
+cas2$FCMvsF_cas<- round((cas2$M_cas_avg/cas2$F_cas_avg), digits = 2)
 cas2$Log2MvsF_cas<- round(log2(cas2$M_cas_avg/cas2$F_cas_avg), digits = 2)
 
 #NZO
@@ -120,6 +125,7 @@ colnames(nzo)<- c("F6_M_NZO", "F7_F_NZO", "F9_M_NZO", "F10_F_NZO")
 nzo2<- nzo
 nzo2$M_nzo_avg<- rowMeans(subset(nzo, select = c(F6_M_NZO, F9_M_NZO)), na.rm = TRUE)
 nzo2$F_nzo_avg<- rowMeans(subset(nzo, select = c(F7_F_NZO, F10_F_NZO)), na.rm = TRUE)
+nzo2$FCMvsF_nzo<- round((nzo2$M_nzo_avg/nzo2$F_nzo_avg), digits = 2)
 nzo2$Log2MvsF_nzo<- round(log2(nzo2$M_nzo_avg/nzo2$F_nzo_avg), digits = 2)
 
 #NOD
@@ -131,18 +137,28 @@ colnames(nod)<- c("F7_M_NOD", "F7_F_NOD", "F9_M_NOD", "F9_F_NOD")
 nod2<- nod
 nod2$M_nod_avg<- rowMeans(subset(nod, select = c(F7_M_NOD, F9_M_NOD)), na.rm = TRUE)
 nod2$F_nod_avg<- rowMeans(subset(nod, select = c(F7_F_NOD, F9_F_NOD)), na.rm = TRUE)
+nod2$FCMvsF_nod<- round((nod2$M_nod_avg/nod2$F_nod_avg), digits = 2)
 nod2$Log2MvsF_nod<- round(log2(nod2$M_nod_avg/nod2$F_nod_avg), digits = 2)
 
 #
 #Merge them
 cc_df<- cbind(pwk,c57,aj,s129,wsb,cas,nzo,nod )
-cc_df2<- as.data.frame(cbind(pwk2[,7],c572[,7],aj2[,7],s1292[,7],wsb2[,7],cas2[,7],nzo2[,7],nod2[,7]))
+cc_df2<- as.data.frame(cbind(pwk2[,8],c572[,8],aj2[,8],s1292[,8],wsb2[,8],cas2[,8],nzo2[,8],nod2[,8]))
+cc_df3<- as.data.frame(cbind(pwk2[,7],c572[,7],aj2[,7],s1292[,7],wsb2[,7],cas2[,7],nzo2[,7],nod2[,7]))
+
 rownames(cc_df2)<- genes
 colnames(cc_df2)<- c("PWK", "C57", "AJ", "129", "WSB", "CAS", "NZO", "NOD")
 write.table(cc_df, file = "cc_df.txt", sep = "\t", quote = F, row.names = TRUE, col.names = TRUE)
 write.table(cc_df2, file = "cc_df2.txt", sep = "\t", quote = F, row.names = TRUE, col.names = TRUE)
 
+rownames(cc_df3)<- genes
+colnames(cc_df3)<- c("PWK", "C57", "AJ", "129", "WSB", "CAS", "NZO", "NOD")
+write.table(cc_df3, file = "cc_df3.txt", sep = "\t", quote = F, row.names = TRUE, col.names = TRUE)
 
+
+#Normalize the regular fc m/f so that C57 is 1
+df3test<- as.data.frame(t(cc_df3[rownames(cc_df3)==gene,]))
+df3$strain<- c(rep("PWK",4), rep("C57", 4), rep("AJ", 4), rep("129", 4), rep("WSB", 4), rep("CAS", 4), rep("NZO", 4), rep("NOD", 4))
 
 
 
